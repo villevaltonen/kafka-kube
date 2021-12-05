890086ad49193aef6395dfb9c5e37840d0a4f229
Ville Valtonen <valtonen.vt@gmail.com>
2021-12-05 18:49:56 +0200

    Zk image from scratch

--- zookeeper/docker/start.sh
+++ zookeeper/docker/start.sh
@@ -2,17 +2,15 @@
 ZOOKEEPER_HOME=/home/user/zookeeper
 
 # Set id
-FOO=zk-0
-ZOOKEEPER_SERVER_ID=$((${FOO##*-}+1)) # TODO CHANGE TO HOSTNAME
+ZOOKEEPER_SERVER_ID=$((${HOSTNAME##*-}+1))
 echo $ZOOKEEPER_SERVER_ID >> /home/user/data/myid
 
 # Set servers
 for (( i=0; i<=$ZOOKEEPER_SERVERS; i++ ))
 do  
-    h=${FOO%?}${i}
+    h=${HOSTNAME%?}${i}
     echo server.$((${i}+1))=$h:$SERVER_PORT:$LEADER_ELECTION_PORT >> $ZOOKEEPER_HOME/conf/zoo.cfg
 done
-
 
 # Start
 java -cp "zookeeper-$ZOOKEEPER_VERSION.jar:$ZOOKEEPER_HOME/lib/*:$ZOOKEEPER_HOME/conf" org.apache.zookeeper.server.quorum.QuorumPeerMain $ZOOKEEPER_HOME/conf/zoo.cfg
